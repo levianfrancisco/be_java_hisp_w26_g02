a637f37962beeae9f0f4dbfb1936bc078262a371
jbendezugonz_meli <jonathanleonel.bendezugonzalez@mercadolibre.com>
2024-04-24 10:41:12 -0300

    Add exception handler

---
+++ src/main/java/bootcamp/sprint/grupo02/sprintI/exception/ExceptionController.java
@@ -0,0 +1,19 @@
+package bootcamp.sprint.grupo02.sprintI.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import bootcamp.sprint.grupo02.sprintI.dto.response.MessageResponseDTO;
+
+@RestControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<MessageResponseDTO> notFound(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new MessageResponseDTO(ex.getMessage()));
+    }
+
+}
