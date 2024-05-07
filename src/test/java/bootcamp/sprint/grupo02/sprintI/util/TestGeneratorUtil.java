bf9ebfabcb9060f4388672720a0ac9ff75319679
macarenacaridad <macarena.caridad@mercadolibre.com>
2024-05-07 11:53:16 -0300

    test 4

--- src/test/java/bootcamp/sprint/grupo02/sprintI/util/TestGeneratorUtil.java
+++ src/test/java/bootcamp/sprint/grupo02/sprintI/util/TestGeneratorUtil.java
@@ -3,6 +3,9 @@ package bootcamp.sprint.grupo02.sprintI.util;
 import bootcamp.sprint.grupo02.sprintI.model.Buyer;
 import bootcamp.sprint.grupo02.sprintI.model.Seller;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class TestGeneratorUtil {
 
     public static Seller createSellerWithId(int id) {
@@ -12,10 +15,59 @@ public class TestGeneratorUtil {
                 .build();
     }
 
-    public static Buyer createBullerWithId(int id) {
+    public static Buyer createBuyerWithId(int id) {
         return Buyer.builder()
                 .id(id)
                 .name(String.format("Buyer %d", id))
+                .build();
+    }
+
+    public static List<Buyer> get4FollowersAsc() {
+        return List.of(
+                createBuyerWithId(1),
+                createBuyerWithId(2),
+                createBuyerWithId(3),
+                createBuyerWithId(4)
+        );
+    }
+    public static List<Buyer> get4FollowersDesc() {
+        return List.of(
+                createBuyerWithId(4),
+                createBuyerWithId(3),
+                createBuyerWithId(2),
+                createBuyerWithId(1)
+        );
+    }
+    public static List<Seller> get4FollowedAsc() {
+        return List.of(
+                createSellerWithId(1),
+                createSellerWithId(2),
+                createSellerWithId(3),
+                createSellerWithId(4)
+        );
+    }
+    public static List<Seller> get4FollowedDesc() {
+        return List.of(
+                createSellerWithId(4),
+                createSellerWithId(3),
+                createSellerWithId(2),
+                createSellerWithId(1)
+        );
+    }
+
+    public static Seller createSellerWithFollowers(int id) {
+        return Seller.builder()
+                .id(id)
+                .name(String.format("Seller %d", id))
+                .followers(get4FollowersAsc())
+                .build();
+    }
+
+    public static Buyer createBuyerWithFollowed(int id) {
+        return Buyer.builder()
+                .id(id)
+                .name(String.format("Buyer %d", id))
+                .follows(get4FollowedAsc())
                 .build();
     }
 
