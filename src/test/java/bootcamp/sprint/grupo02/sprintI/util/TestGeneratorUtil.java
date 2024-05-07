84a1ab4a2bb5192cee58ee7177cc27062b87733c
jbendezugonz_meli <jonathanleonel.bendezugonzalez@mercadolibre.com>
2024-05-07 10:56:20 -0300

    Complete(Test): test fecha de los post deberian estar dentro de las ultimas 2 semanas Ok

--- src/test/java/bootcamp/sprint/grupo02/sprintI/util/TestGeneratorUtil.java
+++ src/test/java/bootcamp/sprint/grupo02/sprintI/util/TestGeneratorUtil.java
@@ -1,6 +1,12 @@
 package bootcamp.sprint.grupo02.sprintI.util;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 import bootcamp.sprint.grupo02.sprintI.model.Buyer;
+import bootcamp.sprint.grupo02.sprintI.model.Post;
 import bootcamp.sprint.grupo02.sprintI.model.Seller;
 
 public class TestGeneratorUtil {
@@ -19,4 +25,31 @@ public class TestGeneratorUtil {
                 .build();
     }
 
+    public static List<Seller> createSellers() {
+        return List.of(createSellerWithId(1), createSellerWithId(2));
+    }
+
+    public static List<Integer> createSellersIdList() {
+        return List.of(1, 2);
+    }
+
+    public static List<Post> createPostsBySellerId(int sellerId){
+        LocalDate today = LocalDate.now();
+        LocalDate lastWeek = today.minusWeeks(1);
+        LocalDate twoWeeksAgo = today.minusWeeks(2);
+        LocalDate threeWeeksAgo = today.minusWeeks(3);
+        LocalDate thisWeek = today;
+        LocalDate nextWeek = today.plusWeeks(1);
+        LocalDate[] dates = {today, lastWeek, twoWeeksAgo, threeWeeksAgo, thisWeek, nextWeek};
+    
+        return Arrays.asList(dates)
+        .stream()
+        .map(date -> Post.builder()
+            .sellerId(sellerId)
+            .date(date)
+            .build()
+        )
+        .toList();
+        
+    }
 }
