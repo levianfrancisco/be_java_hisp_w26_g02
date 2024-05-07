369ae00617f5ff5a3446f1e40d69d473b927bc6d
AaChiaravalli <agustin.chiaravalli@mercadolibre.com>
2024-05-07 14:22:46 -0300

    Add #T-0002

--- src/test/java/bootcamp/sprint/grupo02/sprintI/service/implementations/BuyerServiceImplTest.java
+++ src/test/java/bootcamp/sprint/grupo02/sprintI/service/implementations/BuyerServiceImplTest.java
@@ -3,7 +3,11 @@ package bootcamp.sprint.grupo02.sprintI.service.implementations;
 import bootcamp.sprint.grupo02.sprintI.dto.response.FollowedListResponseDTO;
 import bootcamp.sprint.grupo02.sprintI.dto.response.UserResponseDTO;
 import bootcamp.sprint.grupo02.sprintI.exception.BadRequestException;
+import bootcamp.sprint.grupo02.sprintI.exception.NotFoundException;
+import bootcamp.sprint.grupo02.sprintI.model.Buyer;
+import bootcamp.sprint.grupo02.sprintI.model.Seller;
 import bootcamp.sprint.grupo02.sprintI.repository.BuyerRepository;
+import bootcamp.sprint.grupo02.sprintI.repository.SellerRepository;
 import bootcamp.sprint.grupo02.sprintI.util.TestGeneratorUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +20,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,12 +30,14 @@ public class BuyerServiceImplTest {
     @Mock
     private BuyerRepository buyerRepository;
 
+    @Mock
+    private SellerRepository sellerRepository;
     @InjectMocks
     private BuyerServiceImpl buyerService;
 
     @BeforeEach
     public void setup() {
-        when(buyerRepository.findById(1))
+        lenient().when(buyerRepository.findById(1))
                 .thenReturn(Optional.of(TestGeneratorUtil.createBuyerWithIdAndFollows(1)));
     }
 
@@ -94,5 +102,35 @@ public class BuyerServiceImplTest {
         assertDoesNotThrow(() -> {
             buyerService.searchBuyerFollows(1, order);
         });
+    }
+
+
+    @Test
+    @DisplayName("El usuario a dejar de seguir existe.")
+    public void testUnfollowUser_UserExist() {
+        // Arrange
+        Buyer buyer = TestGeneratorUtil.createBuyerWithId(1);
+        Seller seller = TestGeneratorUtil.createSellerWithId(1);
+
+        when(buyerRepository.findById(anyInt())).thenReturn(Optional.ofNullable(buyer));
+        when(sellerRepository.findById(anyInt())).thenReturn(Optional.ofNullable(seller));
+
+        buyerService.followUser(1, 1);
+        buyerService.UnfollowUser(1,1);
+
+        // Assert
+        assertFalse(buyer.getFollows().contains(seller));
+    }
+    @Test
+    @DisplayName("El usuario a dejar de seguir no existe.")
+    public void testUnfollowUser_UserDoesNotExist() {
+        // Arrange
+        int id = 1;
+        when(buyerRepository.findById(id))
+                .thenReturn(Optional.ofNullable(TestGeneratorUtil.createBuyerWithFollowed(id)));
+        when(sellerRepository.findById(anyInt())).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(NotFoundException.class, () -> buyerService.UnfollowUser(1, 1));
     }
 }
